364c015c2a70150721edb8d7cc8194de0ad0df85
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-01-28 08:51:33 +0530

    refactor : added solution

---
+++ solutions/heap/k-closest-points-to-origin.js
@@ -0,0 +1,62 @@
+/*
+
+Sorting
+
+Time - O(nlogn)
+Space - O(n)
+
+*/
+
+const getDistance=(point)=>{
+    return Math.sqrt(Math.pow(point[0],2)+Math.pow(point[1],2));
+}
+
+const kClosest = function(points, k) {
+    
+    points.sort((a,b)=>{
+        if(getDistance(a)<getDistance(b)){
+          return -1;
+        }  
+        return 1;
+    })
+    
+    return points.slice(0,k);
+    
+};
+
+/*
+
+Max Heap
+
+Time - O(nlogk)
+Space - O(k)
+
+*/
+
+const MaxHeap=require("../../data-structures/max-heap.js");
+
+const kClosest2 = function(points, k) {
+    
+    const maxHeap=new MaxHeap();
+    
+    for(let i=0;i<points.length;i++){
+        const currObject={};
+        currObject.key=getDistance(points[i]);
+        currObject.value=[points[i][0],points[i][1]];
+        maxHeap.insert(currObject);
+        
+        if(maxHeap.getSize()>k){
+            maxHeap.deleteMax();
+        }
+    }
+    
+    const output=[];
+    
+    while(maxHeap.getSize()>0){
+        output.push(maxHeap.getMax());
+        maxHeap.deleteMax();
+    }
+    
+    return output;
+    
+};
