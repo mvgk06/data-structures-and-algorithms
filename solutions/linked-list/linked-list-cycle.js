de8cd422ea5ca70750c2e25ba49f454a55929e57
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-02-14 19:29:52 +0530

    refactor : added solution

---
+++ solutions/linked-list/linked-list-cycle.js
@@ -0,0 +1,53 @@
+/* 
+
+Set
+
+Time - O(n)
+Space - O(n)
+
+*/
+
+const hasCycle = function(head) {
+ 
+    const set=new Set();
+    
+    let curr=head;
+    
+    while(curr!=null){
+        if(set.has(curr)){
+            return true;
+        }
+        else{
+            set.add(curr);
+        }
+        curr=curr.next;
+    }
+    
+    return false;
+    
+};
+
+/*
+
+Two pointer
+
+Time - O(n)
+Space - O(1)
+
+*/
+
+const hasCycle = function(head) {
+ 
+    let slow=head, fast=head;
+    
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow===fast){
+            return true;
+        }
+    }
+    
+    return false;
+    
+};
