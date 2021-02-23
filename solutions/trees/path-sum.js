59eedce932e0e80a6cbaa7ea222b14487ffb0413
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-02-23 20:37:47 +0530

    refactor : updated solution

--- solutions/trees/path-sum.js
+++ solutions/trees/path-sum.js
@@ -23,3 +23,44 @@ const hasPathSum = function(root, targetSum) {
     return pathSum(root,targetSum);
 };
 
+/*
+
+BFS
+
+Time - O(n)
+Space - O(n)
+
+*/
+
+const Deque=require("../../data-structures/deque.js");
+
+const hasPathSum2 = function(root, targetSum) {
+    
+    if(root===null){
+        return false;
+    }
+    
+    const queue = new Deque();
+    
+    queue.push([root,targetSum]);
+    
+    while(queue.getSize()>0){
+        const [curr,sum]=queue.getFront();
+        queue.deque();
+        
+        if(curr.left===null && curr.right===null && (sum-curr.val)===0){
+            return true;
+        }
+        
+        if(curr.left!=null){
+            queue.push([curr.left,sum-curr.val]);
+        }
+        
+        if(curr.right!=null){
+            queue.push([curr.right,sum-curr.val]);
+        }
+    }
+    
+    return false;
+    
+};
