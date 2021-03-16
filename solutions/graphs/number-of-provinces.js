507633c7d7e15cc470661f8173234f8909f12e0a
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-03-16 20:31:40 +0530

    refactor : updated solution

--- solutions/graphs/number-of-provinces.js
+++ solutions/graphs/number-of-provinces.js
@@ -7,15 +7,13 @@ Space - O(n)
 
 */
 
-const dfs=(grid,visited,i)=>{
-    
+const dfs=(grid,visited,i)=>{  
+    visited[i]=true;
     for(let j=0;j<grid[i].length;j++){
-        if(!visited[j] && grid[i][j]===1){
-            visited[j]=true;
+        if(grid[i][j]===1 && !visited[j]){
             dfs(grid,visited,j);
         }
     }
-
 }
 
 const findCircleNum = function(isConnected) {
@@ -26,8 +24,55 @@ const findCircleNum = function(isConnected) {
     let result=0;
     for(let i=0;i<visited.length;i++){
         if(!visited[i]){
-            visited[i]=true;
             dfs(isConnected,visited,i);
+            result++;
+        }
+    }
+    
+    return result;
+    
+};
+
+/*
+
+BFS
+
+Time - O(n^2)
+Space - O(n)
+
+*/
+
+const Deque=require("../../data-structures/deque.js");
+
+const bfs=(grid,visited,i)=>{
+    
+    const queue=new Deque();
+    visited[i]=true;
+    queue.push(i);
+    
+    while(queue.getSize()>0){
+        const curr=queue.getFront();
+        queue.deque();
+        
+        for(let j=0;j<grid[curr].length;j++){
+            if(grid[curr][j]===1 && !visited[j]){
+                visited[j]=true;
+                queue.push(j);
+            }
+        }
+    }
+
+}
+
+const findCircleNum2 = function(isConnected) {
+    
+    const visited=new Array(isConnected.length);
+    visited.fill(false);
+    
+    let result=0;
+    for(let i=0;i<visited.length;i++){
+        if(!visited[i]){
+            bfs(isConnected,visited,i);
             result++;
         }
     }
