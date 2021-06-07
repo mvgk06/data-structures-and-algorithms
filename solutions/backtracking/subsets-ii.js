940314287b29868ee60e4c2a5338dd445d746ff5
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-06-07 13:44:52 +0530

    refactor : updated solution

--- solutions/backtracking/subsets-ii.js
+++ solutions/backtracking/subsets-ii.js
@@ -1,13 +1,23 @@
 /*
 
-Backtracking
+Problem
+https://leetcode.com/problems/subsets-ii/
+
+Approach
+- For every element I have two choices 
+    - If the current element is equal to previous element then
+        - If the previous element was already picked for the subset then I have two choices either I pick the current element or I don't pick the current element.
+        - Else I will have only one choice that is to not pick the current element.
+    - Else I have two choices either I pick the current element or I don't pick the current element.
+- After making a choice, recursively solve the smaller sub problems.
+- If the end of the array is reached there are no more choices to make so store the current subset in the result.
 
 Time - O(2^n)
 Space - O(n)
 
 */
 
-const findUniqueSubsets = (nums, result, currIndex, currSubset, prevIncluded) => {
+const findUniqueSubsets = (nums, result, currIndex, currSubset, prevPicked) => {
 
     if (currIndex === nums.length) {
         result.push([...currSubset]);
@@ -15,7 +25,7 @@ const findUniqueSubsets = (nums, result, currIndex, currSubset, prevIncluded) =>
     }
 
     if (currIndex > 0 && nums[currIndex] === nums[currIndex - 1]) {
-        if (prevIncluded) {
+        if (prevPicked) {
             currSubset.push(nums[currIndex]);
             findUniqueSubsets(nums, result, currIndex + 1, currSubset, true);
             currSubset.pop();
