62ca5a89defb95bbc2fcbc8468ff2d06843dd68e
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-06-22 16:50:48 +0530

    refactor : updated solution

--- solutions/dynamic-programming/partition-equal-subset-sum.js
+++ solutions/dynamic-programming/partition-equal-subset-sum.js
@@ -7,11 +7,11 @@ Approach
 
 1. Top down
 - If the sum of the array elements is odd then the partition is not possible.
-- For each index I have two choice either I pick or don't pick the current element.
-- Recursively solve the smaller sub problems and store the solution in an array.
-- If the end of the array is reached then there are no more choices to make, if the sum is 0 then return 1 to indicate that the partition is possible.
-- Else return 0 to indicate that the parition is not possible.
-- If the current sub problem is already computed then return it instead of recomputing them again.
+- For each index, we have two choices either we pick or don't pick the current element.
+- After making the choice, recursively solve the smaller sub-problems and store the solution in an array.
+- If the end of the array is reached if the sum is 0 then return 1 to indicate that the partition is possible.
+- Else return 0 to indicate that the partition is not possible.
+- If the current subproblem is already computed then return it instead of recomputing them again.
 
 Time - O(n*(sum/2))
 Space - O(n*(sum/2))
@@ -19,41 +19,43 @@ Space - O(n*(sum/2))
 2. Bottom up
 - If the sum of the array elements is odd then the partition is not possible.
 - Create a memo array and initialize with base cases.
-- For each index I have two choice either I pick or don't pick the current element.
-- Use the memo to get the solutions of the smaller sub problems.
-- Return the cell (n,sum/2) which contains the solution for the main problem.
+- For each index, we have two choices either we pick or don't pick the current element.
+- Use the memo to get the solutions to the smaller sub-problems.
+- Return the cell (n-1,sum/2) which contains the solution for the main problem.
 
 Time - O(n*(sum/2))
 Space - O(n*(sum/2))
+
+n - number of elements
+sum - total sum
 
 */
 
 /* Top down */
 
-const canPartitionHelper = (nums, currIndex, sum, memo) => {
+const canPartitionHelper = (nums, index, sum, memo) => {
 
-    if (currIndex === nums.length) {
+    if (index === nums.length) {
         if (sum === 0) {
             return 1;
         }
         return 0;
     }
 
-    if (memo[currIndex][sum] != -1) {
-        return memo[currIndex][sum];
+    if (memo[index][sum] != -1) {
+        return memo[index][sum];
     }
 
-    if (nums[currIndex] <= sum) {
-        memo[currIndex][sum] = canPartitionHelper(nums, currIndex + 1, sum - nums[currIndex], memo);
-        if (memo[currIndex][sum] === 1) {
-            return 1;
-        }
-        memo[currIndex][sum] = canPartitionHelper(nums, currIndex + 1, sum, memo);
-        return memo[currIndex][sum];
+    if (nums[index] <= sum) {
+        const pick = canPartitionHelper(nums, index + 1, sum - nums[index], memo);
+        const dontPick = canPartitionHelper(nums, index + 1, sum, memo);
+        memo[index][sum] = (pick || dontPick);
+        return memo[index][sum];
     }
 
-    memo[currIndex][sum] = canPartitionHelper(nums, currIndex + 1, sum, memo);
-    return memo[currIndex][sum];
+    const dontPick = canPartitionHelper(nums, index + 1, sum, memo);
+    memo[index][sum] = dontPick;
+    return memo[index][sum];
 };
 
 const canPartition = function (nums) {
@@ -67,7 +69,7 @@ const canPartition = function (nums) {
         return false;
     }
 
-    const memo = new Array(nums.length + 1);
+    const memo = new Array(nums.length);
     for (let i = 0; i < memo.length; i++) {
         memo[i] = new Array((sum / 2) + 1).fill(-1);
     }
@@ -95,13 +97,18 @@ const canPartition2 = function (nums) {
         return false;
     }
 
-    const memo = new Array(nums.length + 1);
+    const memo = new Array(nums.length);
     for (let i = 0; i < memo.length; i++) {
         memo[i] = new Array((sum / 2) + 1).fill(-1);
     }
 
     for (let j = 0; j < memo[0].length; j++) {
-        memo[0][j] = 0;
+        if (nums[0] === j) {
+            memo[0][j] = 1;
+        }
+        else {
+            memo[0][j] = 0;
+        }
     }
 
     for (let i = 0; i < memo.length; i++) {
@@ -110,8 +117,8 @@ const canPartition2 = function (nums) {
 
     for (let i = 1; i < memo.length; i++) {
         for (let j = 1; j < memo[i].length; j++) {
-            if (nums[i - 1] <= j) {
-                memo[i][j] = (memo[i - 1][j - nums[i - 1]] || memo[i - 1][j]);
+            if (nums[i] <= j) {
+                memo[i][j] = (memo[i - 1][j - nums[i]] || memo[i - 1][j]);
             }
             else {
                 memo[i][j] = memo[i - 1][j];
@@ -119,7 +126,7 @@ const canPartition2 = function (nums) {
         }
     }
 
-    const result = memo[nums.length][sum / 2];
+    const result = memo[nums.length - 1][sum / 2];
 
     if (result === 1) {
         return true;
--- solutions/dynamic-programming/subset-sum.js
+++ solutions/dynamic-programming/subset-sum.js
@@ -6,23 +6,32 @@ https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
 Approach
 
 1. Top down
-- For each index, I have two choices either I pick or don't pick the current element.
-- Recursively solve the smaller sub problems and store the solutions in an array.
-- If the end of the array is reached, if the sum is equal to 0 then return 1 to indicate that the sum is possible.
+- For each index, we have two choices either we pick or don't pick the current element.
+- After making the choice, recursively solve the smaller sub-problems and store the solutions in an array.
+- If the end of the array is reached if the sum is equal to 0 then return 1 to indicate that the sum is possible.
 - Else return 0 to indicate that the sum is not possible.
-- If the current sub problem is already computed return it instead of recomputing them.
+- If the current subproblem is already computed return it instead of recomputing them.
 
 Time - O(n*sum)
 Space - O(n*sum)
 
 2. Bottom up
 - Create a memo array and initialize the base cases.
-- For each index in the memo, I have two choices either I pick or don't pick the current element.
-- Use the memo to get the solutions of smaller sub problems.
-- Return the cell (n,sum) which contains the solution of the main problem.
+- For each index in the memo, we have two choices either we pick or don't pick the current element.
+- Use the memo to get the solutions of smaller sub-problems.
+- Return the cell (n-1, sum) which contains the solution of the main problem.
 
 Time - O(n*sum)
 Space - O(n*sum)
+
+3. Bottom up (1 array)
+
+
+Time - O(n*sum)
+Space - O(sum)
+
+n - number of elements
+sum - total sum
 
 */
 
@@ -30,38 +39,38 @@ Space - O(n*sum)
 
 class Solution {
 
-    subsetSumHelper(arr, n, sum, currIndex, memo) {
+    subsetSumHelper(arr, sum, index, memo) {
 
-        if (currIndex === n) {
+        if (index === arr.length) {
             if (sum === 0) {
                 return 1;
             }
             return 0;
         }
 
-        if (memo[currIndex][sum] != -1) {
-            return memo[currIndex][sum];
+        if (memo[index][sum] != -1) {
+            return memo[index][sum];
         }
 
-        if (arr[currIndex] <= sum) {
-            memo[currIndex][sum] = this.subsetSumHelper(arr, n, sum - arr[currIndex], currIndex + 1, memo);
+        if (arr[index] <= sum) {
+            const pick = this.subsetSumHelper(arr, sum - arr[index], index + 1, memo);
+            const dontPick = this.subsetSumHelper(arr, sum, index + 1, memo);
+            memo[index][sum] = (pick || dontPick);
+            return memo[index][sum];
         }
 
-        if (memo[currIndex][sum] === 1) {
-            return 1;
-        }
-
-        memo[currIndex][sum] = this.subsetSumHelper(arr, n, sum, currIndex + 1, memo);
-        return memo[currIndex][sum];
+        const dontPick = this.subsetSumHelper(arr, sum, index + 1, memo);
+        memo[index][sum] = dontPick;
+        return memo[index][sum];
 
     }
 
     isSubsetSum(arr, n, sum) {
-        const memo = new Array(n + 1);
+        const memo = new Array(n);
         for (let i = 0; i < memo.length; i++) {
             memo[i] = new Array(sum + 1).fill(-1);
         }
-        const result = this.subsetSumHelper(arr, n, sum, 0, memo);
+        const result = this.subsetSumHelper(arr, sum, 0, memo);
         if (result === 1) {
             return true;
         }
@@ -75,13 +84,18 @@ class Solution {
 class Solution2 {
 
     isSubsetSum(arr, n, sum) {
-        const memo = new Array(n + 1);
+        const memo = new Array(n);
         for (let i = 0; i < memo.length; i++) {
             memo[i] = new Array(sum + 1).fill(-1);
         }
 
         for (let j = 0; j < memo[0].length; j++) {
-            memo[0][j] = 0;
+            if (arr[0] === j) {
+                memo[0][j] = 1;
+            }
+            else {
+                memo[0][j] = 0;
+            }
         }
 
         for (let i = 0; i < memo.length; i++) {
@@ -90,8 +104,8 @@ class Solution2 {
 
         for (let i = 1; i < memo.length; i++) {
             for (let j = 1; j < memo[i].length; j++) {
-                if (arr[i - 1] <= j) {
-                    memo[i][j] = (memo[i - 1][j - arr[i - 1]] || memo[i - 1][j]);
+                if (arr[i] <= j) {
+                    memo[i][j] = (memo[i - 1][j - arr[i]] || memo[i - 1][j]);
                 }
                 else {
                     memo[i][j] = memo[i - 1][j];
@@ -99,7 +113,7 @@ class Solution2 {
             }
         }
 
-        if (memo[n][sum] === 1) {
+        if (memo[n - 1][sum] === 1) {
             return true;
         }
         return false;
