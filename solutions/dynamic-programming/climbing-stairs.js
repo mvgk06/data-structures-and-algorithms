4952f7c54de81b0c2a3e01e2aca54976223e6533
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-06-24 16:16:17 +0530

    refactor : updated solution

--- solutions/dynamic-programming/climbing-stairs.js
+++ solutions/dynamic-programming/climbing-stairs.js
@@ -6,23 +6,25 @@ https://leetcode.com/problems/climbing-stairs/
 Approach
 
 1. Top down
-- For each stair I have two choices either I can go to the 1st stair or the 2nd stair.
-- Recursively solve the smaller sub problems and store the solutions in an array.
+- For each stair, we have two choices either I can go to the 1st stair or the 2nd stair.
+- After making the choice, recursively solve the smaller sub-problems and store the solutions in an array.
 - If the top of the stair is reached then return 1 to indicate that we have found a way to reach the top.
 - If the current stair becomes more than the top then return 0 to indicate that there is no way to reach the top.
-- If the current sub problem is already computed return it instead of recomputing them again.
+- If the current subproblem is already computed return it instead of recomputing them again.
 
 Time - O(n)
 Space - O(n)
 
 2. Bottom up
 - Create a memo array and initialize with base cases.
-- For each stair I have two choices either I could have came from the stair i-1 or i-2.
-- Use the memo to get the solution of the smaller sub problems.
-- Return the nth index of memo which contains the solution for the main problem.
+- For each stair, we have two choices either we can go to the 1st or the 2nd stair.
+- Use the memo to get the solution of the smaller sub-problems.
+- Return the last element of the memo which contains the solution for the main problem.
 
 Time - O(n)
 Space - O(n)
+
+n - number of stairs
 
 */
 
@@ -49,7 +51,7 @@ const climbStairsHelper = (n, currStair, memo) => {
 };
 
 const climbStairs = function (n) {
-    const memo = new Array(n + 1).fill(-1);
+    const memo = new Array(n).fill(-1);
     return climbStairsHelper(n, 0, memo);
 };
 
--- solutions/dynamic-programming/house-robber.js
+++ solutions/dynamic-programming/house-robber.js
@@ -6,60 +6,62 @@ https://leetcode.com/problems/house-robber/
 Approach
 
 1. Top down
-- For every index I have two choices I can either rob the house or don't rob the house.
-- Recursively solve the smaller sub problems, store the solution of the sub problem in an array and return it.
-- If the current index goes out of bounds return 0 to indicate that no money can be robbed.
-- If the current sub problem is already computed return the computed solution instead of recomputing them. 
+- For every index, we have two choices we can either rob or don't rob the house.
+- After making the choice, recursively solve the smaller sub-problems, and store the solutions in an array.
+- If the index goes out of bounds then return 0 to indicate that no money can be robbed.
+- If the current subproblem is already computed then return it instead of recomputing them. 
 
 Time - O(n)
 Space - O(n)
 
 2. Bottom up
 - Create a memo array and initialize with the base cases.
-- For every index I have two choices I can either rob the house or don't rob the house.
-- Use the memo to get the solutions of smaller sub problems.
-- Return the nth index of memo which contains the solution for the main problem. 
+- For every index, we have two choices we can either rob or don't rob the house.
+- Use the memo to get the solutions of smaller sub-problems.
+- Return the last element in the memo which contains the solution for the main problem. 
 
 Time - O(n)
 Space - O(n)
+
+n - number of houses
 
 */
 
 /* Top down */
 
-const robHelper = (nums, currIndex, memo) => {
+const robHelper = (nums, index, memo) => {
 
-    if (currIndex >= nums.length) {
+    if (index >= nums.length) {
         return 0;
     }
 
-    if (memo[currIndex] != -1) {
-        return memo[currIndex];
+    if (memo[index] != -1) {
+        return memo[index];
     }
 
-    const robCurr = nums[currIndex] + robHelper(nums, currIndex + 2, memo);
-    const dontRobCurr = robHelper(nums, currIndex + 1, memo);
+    const rob = nums[index] + robHelper(nums, index + 2, memo);
+    const dontRob = robHelper(nums, index + 1, memo);
 
-    memo[currIndex] = Math.max(robCurr, dontRobCurr);
-    return memo[currIndex];
+    memo[index] = Math.max(rob, dontRob);
+    return memo[index];
 
 };
 
 const rob = function (nums) {
-    const memo = new Array(nums.length + 1).fill(-1);
+    const memo = new Array(nums.length).fill(-1);
     return robHelper(nums, 0, memo);
 };
 
 /* Bottom up */
 
 const rob2 = function (nums) {
-    const memo = new Array(nums.length + 1).fill(0);
-    memo[1] = nums[0];
-    memo[2] = Math.max(nums[0], nums[1]);
+    const memo = new Array(nums.length).fill(0);
+    memo[0] = nums[0];
+    memo[1] = Math.max(nums[0], nums[1]);
 
-    for (let i = 3; i < memo.length; i++) {
-        memo[i] = Math.max(nums[i - 1] + memo[i - 2], memo[i - 1]);
+    for (let i = 2; i < memo.length; i++) {
+        memo[i] = Math.max(nums[i] + memo[i - 2], memo[i - 1]);
     }
 
-    return memo[nums.length];
+    return memo[nums.length - 1];
 };
