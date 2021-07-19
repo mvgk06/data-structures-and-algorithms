ff464b4df9096a95b70d7d7e2e3d71120315229a
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-19 10:21:59 +0530

    refactor : updated solution

--- solutions/trees/invert-binary-tree.js
+++ solutions/trees/invert-binary-tree.js
@@ -1,40 +1,50 @@
 /*
 
-DFS
+Problem
+
+https://leetcode.com/problems/invert-binary-tree/
+
+Approach
+
+1. DFS
+- Swap the left and right nodes.
+- Recursively solve the left and right subtree.
+- Return the root.
+- If the root is null then return null.
 
 Time - O(n)
 Space - O(n)
 
+2. BFS
+- Create a queue and push the root.
+- While the queue is not empty, deque the front node and swap the left and right nodes.
+- If the left, right is not null then push it into the queue.
+- Return the root.
+
+Time - O(n)
+Space - O(n)
+
+n - number of nodes
+
 */
 
-const invert = (curr) => {
-  if (curr === null) {
-    return;
-  }
-
-  const temp = curr.left;
-  curr.left = curr.right;
-  curr.right = temp;
-
-  invert(curr.left);
-  invert(curr.right);
-};
+/* DFS */
 
 const invertTree = function (root) {
-  invert(root);
+  if (root === null) {
+    return null;
+  }
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
   return root;
 };
 
-/*
+/* BFS */
 
-BFS
-
-Time - O(n)
-Space - O(n)
-
-*/
-
-const Deque=require("../../data-structures/deque.js");
+const Deque = require("../../data-structures/deque.js");
 
 const invertTree2 = function (root) {
   if (root === null) {
@@ -45,8 +55,8 @@ const invertTree2 = function (root) {
   queue.push(root);
 
   while (queue.getSize() > 0) {
-    
-    const curr = queue.getFront(),left = curr.left,right = curr.right;
+
+    const curr = queue.getFront(), left = curr.left, right = curr.right;
     queue.deque();
 
     curr.left = right;
