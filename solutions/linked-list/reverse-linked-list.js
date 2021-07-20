7d9223584b9bd1ea2093f0a4cccb5c1fbd200422
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-20 07:54:06 +0530

    refactor : updated solution

--- solutions/linked-list/reverse-linked-list.js
+++ solutions/linked-list/reverse-linked-list.js
@@ -1,48 +1,54 @@
 /*
 
-Two pointer - Iterative
+Problem
+
+https://leetcode.com/problems/reverse-linked-list/
+
+Approach
+
+1. Recursion
+- Make the current's next point to prev.
+- Recursive reverse the smaller linked list.
+- If the current is null, then return previous.
+
+Time - O(n)
+Space - O(n)
+
+2. Iterative
+- Initialize the previous as null, current as head.
+- While current is not null, make the current's next point to prev.
+- Update positions of previous and current.
+- Return previous.
 
 Time - O(n)
 Space - O(1)
 
 */
 
-const reverseList = function(head) {
-    
-    let prev=null, curr=head;
-    
-    while(curr!=null){
-        const next=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=next;
-    }
-    
-    return prev;
-    
-};
+/* Recursion */
 
-/*
-
-Two pointer - Recursive
-
-Time - O(n)
-Space - O(n)
-
-*/
-
-const reverse=(prev,curr)=>{
-    
-    if(curr===null){
+const reverse = (prev, curr) => {
+    if (curr === null) {
         return prev;
     }
-    
-    const next=curr.next;
-    curr.next=prev;
-    return reverse(curr,next);
-    
-}
+    const next = curr.next;
+    curr.next = prev;
+    return reverse(curr, next);
+};
 
-const reverseList2 = function(head) {
-    return reverse(null,head);
+const reverseList = function (head) {
+    return reverse(null, head);
+};
+
+/* Iterative */
+
+const reverseList = function (head) {
+    let prev = null, curr = head;
+    while (curr != null) {
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 };
