eb7a024fdd6ec6d132160c387152ba2a2e9abe84
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-20 10:08:54 +0530

    refactor : updated solution

--- solutions/trees/same-tree.js
+++ solutions/trees/same-tree.js
@@ -1,75 +1,73 @@
 /*
 
-DFS
+Problem
+
+https://leetcode.com/problems/same-tree/
+
+Approach
+
+1. DFS
+- Recursively check if the left and right subtree are same.
+- If both the nodes are null, then return true.
+- If either of the nodes are null or not equal, then return false.
 
 Time - O(n)
 Space - O(n)
 
+2. BFS
+- Push both the nodes into the queue.
+- While the queue is not empty, deque first two nodes. 
+- If both the nodes are null, then continue.
+- If either of the nodes are null or not equal, then return false.
+- Push left, right child nodes of both the nodes into the queue.
+- Return true.
+
+Time - O(n)
+Space - O(n)
+
+n - number of nodes
+
 */
 
-const isSame=(curr1,curr2)=>{
-    
-    if(curr1===null && curr2===null){
+/* DFS */
+
+const isSame = (curr1, curr2) => {
+    if (curr1 === null && curr2 === null) {
         return true;
     }
-    
-    if(curr1===null || curr2===null){
+    if (curr1 === null || curr2 === null || curr1.val != curr2.val) {
         return false;
     }
-    
-    return curr1.val===curr2.val && isSame(curr1.left,curr2.left) && isSame(curr1.right,curr2.right);
-    
-}
-
-const isSameTree = function(p, q) {
-    return isSame(p,q);
+    return isSame(curr1.left, curr2.left) && isSame(curr1.right, curr2.right);
 };
 
-/*
+const isSameTree = function (p, q) {
+    return isSame(p, q);
+};
 
-BFS
+/* BFS */
 
-Time - O(n)
-Space - O(n)
+const Deque = require("../../data-structures/deque.js");
 
-*/
-
-const Deque=require("../../data-structures/deque.js");
-
-const isSameTree2 = function(p, q) {
-    
-    if(p===null && q===null){
-        return true;
-    }
-    
-    if(p===null || q===null){
-        return false;
-    }
-    
-    const queue=new Deque();
+const isSameTree2 = function (p, q) {
+    const queue = new Deque();
     queue.push(p);
     queue.push(q);
-    
-    while(queue.getSize()>0){
-        const curr1=queue.getFront();
+    while (queue.getSize() > 0) {
+        const curr1 = queue.getFront();
         queue.deque();
-        const curr2=queue.getFront();
+        const curr2 = queue.getFront();
         queue.deque();
-        
-        if(curr1===null && curr2===null){
+        if (curr1 === null && curr2 === null) {
             continue;
         }
-        
-        else if(curr1===null || curr2===null || curr1.val!=curr2.val){
+        else if (curr1 === null || curr2 === null || curr1.val != curr2.val) {
             return false;
         }
-
         queue.push(curr1.left);
         queue.push(curr2.left);
         queue.push(curr1.right);
         queue.push(curr2.right);
     }
-    
     return true;
-    
 };
