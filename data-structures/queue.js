9efdb15a1862abbe7838ff19feba0eaabd62cc65
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-22 16:04:33 +0530

    refactor : added implementation

---
+++ data-structures/queue.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+    enque(value) {
+        const element = new Node(value);
+        if (this.size === 0) {
+            this.front = element;
+            this.rear = element;
+        } else {
+            this.rear.next = element;
+            this.rear = element;
+        }
+        this.size++;
+    }
+    deque() {
+        if (this.size !== 0) {
+            this.front = this.front.next;
+            this.size--;
+        }
+    }
+    getFront() {
+        return this.front ? this.front.value : null;
+    }
+    getRear() {
+        return this.rear ? this.rear.value : null;
+    }
+    getSize() {
+        return this.size;
+    }
+    * generator() {
+        let curr = this.front;
+        while (curr != null) {
+            yield curr.value;
+            curr = curr.next;
+        }
+    }
+}
+
+module.exports = Queue;
