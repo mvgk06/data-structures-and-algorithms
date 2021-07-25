9296a5346ed75afa5917ac15ad64048337db4857
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-25 16:09:11 +0530

    refactor : updated solution

--- solutions/linked-list/middle-of-the-linked-list.js
+++ solutions/linked-list/middle-of-the-linked-list.js
@@ -1,52 +1,54 @@
 /* 
 
-Brute force
+Problem
+
+https://leetcode.com/problems/middle-of-the-linked-list/
+
+Approach
+
+1. Count the nodes
+- Count the total number of nodes in the linked list.
+- Return the middle node.
 
 Time - O(n)
 Space - O(1)
 
+2. Slow and fast pointers
+- Initialize the slow and fast pointer as head.
+- Move the slow pointer by 1 node and fast pointer by 2 nodes.
+- Return the slow pointer which is the middle node.
+
+Time - O(n)
+Space - O(1)
+
+n - size of the linked list
+
 */
 
-const middleNode = function(head) {
-    
-    let curr=head, count=0;
-    
-    while(curr!=null){
-        count+=1;
-        curr=curr.next;
+/* Count the nodes */
+
+const middleNode = function (head) {
+    let curr = head, count = 0;
+    while (curr != null) {
+        count += 1;
+        curr = curr.next;
     }
-    
-    let midIndex=Math.floor(count/2);
-    
-    curr=head, count=0;
-    
-    while(curr!=null && count<midIndex){
-        count+=1;
-        curr=curr.next;
+    let midIndex = Math.floor(count / 2);
+    curr = head, count = 0;
+    while (curr != null && count < midIndex) {
+        count += 1;
+        curr = curr.next;
     }
-    
     return curr;
-    
 };
 
-/* 
+/* Slow and fast pointers */
 
-Two pointer
-
-Time - O(n)
-Space - O(1)
-
-*/
-
-const middleNode2 = function(head) {
-    
-    let slow=head, fast=head;
-    
-    while(fast!=null && fast.next!=null){
-        slow=slow.next
-        fast=fast.next.next;
+const middleNode2 = function (head) {
+    let slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-
     return slow;
-    
 };
--- solutions/linked-list/palindrome-linked-list.js
+++ solutions/linked-list/palindrome-linked-list.js
@@ -1,42 +1,84 @@
 /*
 
-Two pointer 
+Problem
+
+https://leetcode.com/problems/palindrome-linked-list/
+
+Approach
+
+1. Reverse first half
+- Count the total number of nodes in the linked list.
+- Reverse the first half nodes and check if the first and second halves are equal.
 
 Time - O(n)
 Space - O(1)
 
+2. Reverse second half
+- Find the middle of the linked list.
+- Reverse the second half nodes and check if the first and second halves are equal.
+
+Time - O(n)
+Space - O(1)
+
+n - size of the linked list
+
 */
 
-const isPalindrome = function(head) {
-    
-    if(!head || head.next===null){
-        return true;
+/* Reverse first half */
+
+const isPalindrome = function (head) {
+    let count = 0, curr = head;
+    while (curr != null) {
+        count++;
+        curr = curr.next;
     }
-    
-    let slow=head, fast=head;
-    while(fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
+    const isOdd = (count % 2 != 0);
+    const limit = Math.floor(count / 2);
+    let prev = null;
+    curr = head;
+    count = 0;
+    while (count < limit) {
+        count++;
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
     }
-    
-    let curr=slow, prev=null;
-    
-    while(curr!=null){
-        const next=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=next;
+    if (isOdd) {
+        curr = curr.next;
     }
-    
-    curr=head;
-    while(prev!=null && curr!=null){
-        if(prev.val!=curr.val){
+    while (prev != null && curr != null) {
+        if (prev.val != curr.val) {
             return false;
         }
-        prev=prev.next;
-        curr=curr.next;
+        prev = prev.next;
+        curr = curr.next;
     }
-    
     return true;
-    
+};
+
+/* Reverse second half */
+
+const isPalindrome2 = function (head) {
+    let slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let curr = slow, prev = null;
+    while (curr != null) {
+        const next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    curr = head;
+    while (prev != null && curr != null) {
+        if (prev.val != curr.val) {
+            return false;
+        }
+        prev = prev.next;
+        curr = curr.next;
+    }
+    return true;
 };
