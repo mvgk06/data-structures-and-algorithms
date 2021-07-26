656fe1c20b766bc140a88049199565d5f1b055c9
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-07-26 06:36:18 +0530

    refactor : updated solution

--- solutions/binary-search/koko-eating-bananas.js
+++ solutions/binary-search/koko-eating-bananas.js
@@ -1,23 +1,25 @@
 /*
 
 Problem
+
 https://leetcode.com/problems/koko-eating-bananas/
 
 Approach
 - Consider the range from 1 to maximum element as search space.
 - Divide the range into two.
-- If using the mid as number of hours it is possible to eat all the bananas then mark it as potential result and search on the left subarray.
+- If using the mid as the number of hours if it is possible to eat all the bananas then mark it as potential result and search on the left subarray.
 - Else search on the right subarray.
 
 Time - O(nlog(max))
 Space - O(1)
 
+n - number of elements
+max - maximum element
+
 */
 
 const isPossibleToEat = (piles, h, mid) => {
-
     let hrsSpentToEat = 0;
-
     for (let i = 0; i < piles.length; i++) {
         if (piles[i] < mid) {
             hrsSpentToEat++;
@@ -29,19 +31,14 @@ const isPossibleToEat = (piles, h, mid) => {
             return false;
         }
     }
-
     return true;
-
 };
 
 const minEatingSpeed = function (piles, h) {
-
     let max = -Number.MAX_VALUE;
-
     for (let i = 0; i < piles.length; i++) {
         max = Math.max(max, piles[i]);
     }
-
     let start = 1, end = max, result = -1;
     while (start <= end) {
         const mid = Math.floor(start + (end - start) / 2);
@@ -53,6 +50,5 @@ const minEatingSpeed = function (piles, h) {
             start = mid + 1;
         }
     }
-
     return result;
 };
--- solutions/binary-search/minimum-number-of-days-to-make-m-bouquets.js
+++ solutions/binary-search/minimum-number-of-days-to-make-m-bouquets.js
@@ -1,22 +1,25 @@
 /* 
 
 Problem
+
 https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
 
 Approach
-- Consider the range from 1 to maximum element as the search space.
+- Consider the range from 1 to maximum day as the search space.
 - Divide the range into two.
-- If using the mid as number of days it is possible to make m bouquets then mark the mid as potential result and search on the left subarray.
+- If using the mid as number of days if it is possible to make m bouquets then mark the mid as potential result and search on the left subarray.
 - Else search on the right subarray.
+- Return the result.
 
 Time - O(nlog(max))
 Space - O(1)
 
+n - number of elements
+max - maximum day
+
 */
 
-
 const isBouquetPossible = (bloomDay, m, k, mid) => {
-
     let flowers = 0, bouquets = 0;
     for (let i = 0; i < bloomDay.length; i++) {
         if (mid >= bloomDay[i]) {
@@ -33,18 +36,14 @@ const isBouquetPossible = (bloomDay, m, k, mid) => {
             return true;
         }
     }
-
     return false;
 };
 
 const minDays = function (bloomDay, m, k) {
-
     let max = -Number.MAX_VALUE;
-
     for (let i = 0; i < bloomDay.length; i++) {
         max = Math.max(max, bloomDay[i]);
     }
-
     let start = 1, end = max, result = -1;
     while (start <= end) {
         const mid = Math.floor(start + (end - start) / 2);
@@ -56,6 +55,5 @@ const minDays = function (bloomDay, m, k) {
             start = mid + 1;
         }
     }
-
     return result;
 };
