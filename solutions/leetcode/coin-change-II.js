7e35deda0ce101e4046839f0b6d182c705928011
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-08-09 19:36:49 +0530

    added solution

---
+++ solutions/leetcode/coin-change-II.js
@@ -0,0 +1,118 @@
+/*
+
+Problem
+
+https://leetcode.com/problems/coin-change-2/
+
+Approach
+
+1. Top down
+- Each state in the memo[i][j] represents the number of ways to get the amount j if we can pick elements till the ith index.
+- For each index, we have two choices either we can pick or don't pick the current coin.
+- After making the choice, recursively solve the smaller sub-problems and store the solution in the memo.
+- If the index is out of bounds if the amount is 0 return 1 to indicate that we have found a way.
+- Else return 0 to indicate that no way is possible.
+- If the current subproblem is already computed then return it instead of recomputing them.
+
+Time - O(n*amount)
+Space - O(n*amount)
+
+2. Bottom up
+- Create a memo array and initialize with base cases.
+- Each state in the memo[i][j] represents the number of ways to get the amount j if the size of input is i.
+- For each index, we have two choices either we can pick or don't pick the current coin.
+- Use the memo to get the solutions to the smaller sub-problems.
+- Return the cell (n, amount) which contains the solution to the main problem.
+
+Time - O(n*amount)
+Space - O(n*amount)
+
+3. Bottom up (space optimized)
+- To compute the current row, we need the solutions only from the current and the previous row. 
+- So we can just have two arrays (previous and current) to keep track of the solution of the subproblems.
+- Use the current and the previous row to get the solution of the current row, then copy the contents of the current row into the previous row.
+- Return the last element which contains the solution of the main problem.
+
+Time - O(n*amount)
+Space - O(amount)
+
+n - number of coins
+amount - total amount
+
+*/
+
+/* Top down */
+
+const changeHelper = (coins, amt, i, memo) => {
+    if (i < 0) {
+        if (amt === 0) {
+            return 1;
+        }
+        return 0;
+    }
+    if (memo[i][amt] != -1) {
+        return memo[i][amt];
+    }
+    if (coins[i] <= amt) {
+        const pick = changeHelper(coins, amt - coins[i], i, memo);
+        const dontPick = changeHelper(coins, amt, i - 1, memo);
+        memo[i][amt] = pick + dontPick;
+    }
+    else {
+        const dontPick = changeHelper(coins, amt, i - 1, memo);
+        memo[i][amt] = dontPick;
+    }
+    return memo[i][amt];
+};
+
+const change = function (amount, coins) {
+    const memo = new Array(coins.length);
+    for (let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(amount + 1).fill(-1);
+    }
+    return changeHelper(coins, amount, coins.length - 1, memo);
+};
+
+/* Bottom up */
+
+const change2 = function (amount, coins) {
+    const memo = new Array(coins.length + 1);
+    for (let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(amount + 1).fill(0);
+    }
+    for (let i = 0; i < memo.length; i++) {
+        memo[i][0] = 1;
+    }
+    for (let i = 1; i < memo.length; i++) {
+        for (let j = 1; j < memo[i].length; j++) {
+            if (coins[i - 1] <= j) {
+                memo[i][j] = memo[i][j - coins[i - 1]] + memo[i - 1][j];
+            }
+            else {
+                memo[i][j] = memo[i - 1][j];
+            }
+        }
+    }
+    return memo[coins.length][amount];
+};
+
+/* Bottom up (space optimized) */
+
+const change3 = function (amount, coins) {
+    let prevMemo = new Array(amount + 1).fill(0);
+    prevMemo[0] = 1;
+    for (let i = 1; i <= coins.length; i++) {
+        const currMemo = new Array(amount + 1);
+        currMemo[0] = 1;
+        for (let j = 1; j <= amount; j++) {
+            if (coins[i - 1] <= j) {
+                currMemo[j] = currMemo[j - coins[i - 1]] + prevMemo[j];
+            }
+            else {
+                currMemo[j] = prevMemo[j];
+            }
+        }
+        prevMemo = [...currMemo];
+    }
+    return prevMemo[amount];
+};
---
+++ solutions/leetcode/coin-change.js
@@ -0,0 +1,133 @@
+/*
+
+Problem
+
+https://leetcode.com/problems/coin-change/
+
+Approach
+
+1. Top down
+- Each state in the memo[i][j] represents the minimum coins required to get the amount j if we can pick elements till ith index.
+- For each index, we have two choices either we pick or don't pick the current coin.
+- After making the choice, recursively solve the smaller sub-problems and store the solution in the memo.
+- If index is out of bounds, if the amount is 0 then the minimum number of coins required is 0.
+- Else it is not possible to get the required amount so return a maximum number.
+- If the current subproblem is already computed then return it instead of recomputing them again.
+
+Time - O(n*amount)
+Space - O(n*amount)
+
+2. Bottom up
+- Create a memo array and initialize the base cases.
+- Each state in the memo[i][j] represents the minimum coins required to get the amount j if the size of the input is i.
+- For each index, we have two choices either we pick or don't pick the current coin.
+- Use the memo to get the solution of the smaller sub-problems.
+- Return the cell (n, amount) which contains the solution for the main problem.
+
+Time - O(n*amount)
+Space - O(n*amount)
+
+3. Bottom up (space optimized)
+- To compute the current row, we need the solutions only from the current and the previous row. 
+- So we can just have two arrays (previous and current) to keep track of the solution of the subproblems.
+- Use the current and the previous row to get the solution of the current row, then copy the contents of the current row into the previous row.
+- Return the last element which contains the solution of the main problem.
+
+Time - O(n*amount)
+Space - O(amount)
+
+n - number of elements
+amount - total amount
+
+*/
+
+/* Top down */
+
+const coinChangeHelper = (coins, amt, i, memo) => {
+    if (i < 0) {
+        if (amt === 0) {
+            return 0;
+        }
+        return Number.MAX_VALUE;
+    }
+    if (memo[i][amt] != -1) {
+        return memo[i][amt];
+    }
+    if (coins[i] <= amt) {
+        const pick = 1 + coinChangeHelper(coins, amt - coins[i], i, memo);
+        const dontPick = coinChangeHelper(coins, amt, i - 1, memo);
+        memo[i][amt] = Math.min(pick, dontPick);
+    }
+    else {
+        const dontPick = coinChangeHelper(coins, amt, i - 1, memo);
+        memo[i][amt] = dontPick;
+    }
+    return memo[i][amt];
+};
+
+const coinChange = function (coins, amount) {
+    const memo = new Array(coins.length);
+    for (let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(amount + 1).fill(-1);
+    }
+    const result = coinChangeHelper(coins, amount, coins.length - 1, memo);
+    if (result != Number.MAX_VALUE) {
+        return result;
+    }
+    return -1;
+};
+
+/* Bottom up */
+
+const coinChange2 = function (coins, amount) {
+    const memo = new Array(coins.length + 1);
+    for (let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(amount + 1).fill(-1);
+    }
+    for (let j = 0; j < memo[0].length; j++) {
+        memo[0][j] = Number.MAX_VALUE;
+    }
+    for (let i = 0; i < memo.length; i++) {
+        memo[i][0] = 0;
+    }
+    for (let i = 1; i < memo.length; i++) {
+        for (let j = 1; j < memo[i].length; j++) {
+            if (coins[i - 1] <= j) {
+                memo[i][j] = Math.min(1 + memo[i][j - coins[i - 1]], memo[i - 1][j]);
+            }
+            else {
+                memo[i][j] = memo[i - 1][j];
+            }
+        }
+    }
+    const result = memo[coins.length][amount];
+    if (result != Number.MAX_VALUE) {
+        return result;
+    }
+    return -1;
+};
+
+/* Bottom up (space optimized) */
+
+const coinChange3 = function (coins, amount) {
+    let prevMemo = new Array(amount + 1).fill(Number.MAX_VALUE);
+    prevMemo[0] = 0;
+    for (let i = 1; i <= coins.length; i++) {
+        const currMemo = new Array(amount + 1);
+        currMemo[0] = 0;
+        for (let j = 1; j <= amount; j++) {
+            if (coins[i - 1] <= j) {
+                currMemo[j] = Math.min(1 + currMemo[j - coins[i - 1]], prevMemo[j]);
+            }
+            else {
+                currMemo[j] = prevMemo[j];
+            }
+        }
+        prevMemo = [...currMemo];
+    }
+    const result = prevMemo[amount];
+    if (result != Number.MAX_VALUE) {
+        return result;
+    }
+    return -1;
+};
