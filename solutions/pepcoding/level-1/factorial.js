710fb05ba29d8fcfde9e9f914557982a9a052815
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-10-01 08:12:40 +0530

    added solution

---
+++ solutions/pepcoding/level-1/factorial.js
@@ -0,0 +1,28 @@
+/*
+
+Problem
+
+https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/factorial-official/ojquestion
+
+Approach
+- Multiply the current number with the factorial of smaller number (recursively solve for smaller sub problems).
+- If the current number is 0, then return 1.
+
+Time - O(n)
+Space - O(n)
+
+n - number
+
+*/
+
+const helper = (n) => {
+    if (n === 0) {
+        return 1;
+    }
+    return n * helper(n - 1);
+};
+
+const solve = (n) => {
+    const result = helper(n);
+    console.log(result);
+};
---
+++ solutions/pepcoding/level-1/print-increasing.js
@@ -0,0 +1,29 @@
+/*
+
+Problem
+
+https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/print-increasing-official/ojquestion
+
+Approach
+- Recursively solve for smaller sub problems.
+- Print the current number.
+- If the number is equal to 0, then return.
+
+Time - O(n)
+Space - O(n)
+
+n - number
+
+*/
+
+const helper = (n) => {
+    if (n === 0) {
+        return;
+    }
+    helper(n - 1);
+    console.log(n);
+};
+
+const solve = (n) => {
+    helper(n);
+};
