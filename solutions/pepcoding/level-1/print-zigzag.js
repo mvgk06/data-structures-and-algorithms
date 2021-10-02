8cebb7eed76e9391faa1813a5da0c7ea13438bb3
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-10-02 06:39:57 +0530

    added solution

---
+++ solutions/pepcoding/level-1/print-zigzag.js
@@ -0,0 +1,31 @@
+/*
+
+Problem
+
+https://www.pepcoding.com/resources/online-java-foundation/introduction-to-recursion/print-zig-zag-official/ojquestion
+
+Approach
+- Print the current number on the left, mid and right of the pattern for smaller sub problem (recursively solve for smaller sub-problems).
+- If the current number is equal to 0, then return.
+
+Time - O(n)
+Space - O(n)
+
+n - number
+
+*/
+
+const helper = (n) => {
+    if (n === 0) {
+        return;
+    }
+    process.stdout.write(`${n} `);
+    helper(n - 1);
+    process.stdout.write(`${n} `);
+    helper(n - 1);
+    process.stdout.write(`${n} `);
+};
+
+const solve = (n) => {
+    helper(n);
+};
