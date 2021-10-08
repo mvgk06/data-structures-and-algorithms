22a0c97ebfdc2d1769fdf6b993b7a62c7b6ffe29
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-10-08 17:20:45 +0530

    updated solution

--- solutions/pepcoding/level-1/print-maze-paths.js
+++ solutions/pepcoding/level-1/print-maze-paths.js
@@ -5,19 +5,19 @@ Problem
 https://www.pepcoding.com/resources/online-java-foundation/recursion-on-the-way-up/print-maze-paths-official/ojquestion
 
 Approach
-- At each index, we have two choices either we can go horizontal (right) or vertical (down).
+- At each index, we have two choices either we can go horizontal (right) or vertical (bottom).
 - After making a choice, recursively solve for the smaller sub problems.
 - Backtrack, undo the choice that was made (this will be taken care by recursion as numbers are passed by value) and try other choices.
 - If cell (n-1,m-1) is reached, then push the current path into the result and return.
 - If out of bounds, then return.
 
-Time - O(2^l)
-Space - O(l+k)
+Time - O(2^i)
+Space - O(i+j)
 
 n - number of rows
 m - number of columns
-l - length of the longest path
-k - number of paths
+i - length of the longest path
+j - number of paths
 
 */
 
