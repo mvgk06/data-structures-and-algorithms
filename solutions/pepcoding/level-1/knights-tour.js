2808b2122d29c1fc6a586a99d81d1452fa5e1292
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-10-11 20:36:55 +0530

    updated solution

--- solutions/pepcoding/level-1/knights-tour.js
+++ solutions/pepcoding/level-1/knights-tour.js
@@ -2,56 +2,57 @@
 
 Problem
 
-https://www.pepcoding.com/resources/online-java-foundation/graphs/knights-tour-official/ojquestion
+https://www.pepcoding.com/resources/online-java-foundation/recursion-backtracking/knights-tour-official/ojquestion
 
 Approach
-- At each cell, we have 8 choices to make.
-- Recursively choose each choice and visit the cells.
+- At each cell, we have 8 choices to place the knight.
+- After making a choice, recursively solve for the smaller sub problems.
 - Backtrack undo the choice that was made and try other choices.
-- If all the cells are visited then print the moves made.
+- If the cell becomes out of bounds or it is already visited, then return.
+- If all the cells are visited, then print the moves made and return.
 
 Time - O(8^(n^2))
 Space - O(n^2)
 
-n - number of cells
+n - number of rows and columns
 
 */
 
-const printGraph = (graph) => {
-    const n = graph.length, m = graph[0].length;
+const printBoard = (board) => {
+    const n = board.length, m = board[0].length;
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m - 1; j++) {
-            process.output.write(graph[i][j].toString() + " ");
+            process.stdout.write(board[i][j].toString() + " ");
         }
-        process.output.write(graph[i][m - 1].toString());
-        process.output.write("\n");
+        console.log(board[i][m - 1].toString());
     }
-    process.output.write("\n");
+    console.log("");
 };
 
-const helper = (graph, n, i, j, paths, move) => {
-    if (i < 0 || i >= n || j < 0 || j >= n || graph[i][j] !== -1) {
+const moveKnight = (board, i, j, paths, move) => {
+    if (i < 0 || i >= board.length || j < 0 || j >= board.length || board[i][j] !== -1) {
         return;
     }
-    if (move === n * n) {
-        graph[i][j] = move;
-        printGraph(graph);
-        graph[i][j] = -1;
+    if (move === board.length * board.length) {
+        board[i][j] = move;
+        printBoard(board);
+        board[i][j] = -1;
         return;
     }
-    graph[i][j] = move;
+    board[i][j] = move;
     for (let k = 0; k < paths.length; k++) {
         const row = i + paths[k][0], col = j + paths[k][1];
-        helper(graph, n, row, col, paths, move + 1);
+        moveKnight(board, row, col, paths, move + 1);
     }
-    graph[i][j] = -1;
+    board[i][j] = -1;
+    return;
 };
 
 const solve = (n, i, j) => {
-    const graph = new Array(n);
+    const board = new Array(n);
     for (let i = 0; i < n; i++) {
-        graph[i] = new Array(n).fill(-1);
+        board[i] = new Array(n).fill(-1);
     }
     const paths = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]];
-    helper(graph, n, i, j, paths, 1);
+    moveKnight(board, i, j, paths, 1);
 };
