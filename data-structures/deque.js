4e93a4d47e5b7f075e9f74226125682021bca37c
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-10-28 10:49:40 +0530

    updated data structure

--- data-structures/deque.js
+++ data-structures/deque.js
@@ -1,68 +1,68 @@
 class Node {
-  constructor(value, prev = null, next = null) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
-  }
+	constructor(value, prev = null, next = null) {
+		this.value = value;
+		this.next = next;
+		this.prev = prev;
+	}
 }
 
 class Deque {
-  constructor() {
-    this.front = null;
-    this.rear = null;
-    this.size = 0;
-  }
-  push(value) {
-    const element = new Node(value);
-    if (this.size === 0) {
-      this.front = element;
-      this.rear = element;
-    } else {
-      this.rear.next = element;
-      element.prev = this.rear;
-      this.rear = element;
-    }
-    this.size++;
-  }
-  pop() {
-    if (this.size !== 0) {
-      this.rear = this.rear.prev;
-      if (this.rear) {
-        this.rear.next = null;
-      }
-      this.size--;
-    }
-  }
-  enque(value) {
-    const element = new Node(value);
-    if (this.size === 0) {
-      this.front = element;
-      this.rear = element;
-    } else {
-      element.next = this.front;
-      this.front.prev = element;
-      this.front = element;
-    }
-    this.size++;
-  }
-  deque() {
-    if (this.size !== 0) {
-      this.front = this.front.next;
-      if (this.front) {
-        this.front.prev = null;
-      }
-      this.size--;
-    }
-  }
-  getFront() {
-    return this.front ? this.front.value : null;
-  }
-  getRear() {
-    return this.rear ? this.rear.value : null;
-  }
-  getSize() {
-    return this.size;
-  }
+	constructor() {
+		this.front = null;
+		this.rear = null;
+		this.size = 0;
+	}
+	push(value) {
+		const element = new Node(value);
+		if (this.size === 0) {
+			this.front = element;
+			this.rear = element;
+		} else {
+			this.rear.next = element;
+			element.prev = this.rear;
+			this.rear = element;
+		}
+		this.size++;
+	}
+	pop() {
+		if (this.size !== 0) {
+			this.rear = this.rear.prev;
+			if (this.rear) {
+				this.rear.next = null;
+			}
+			this.size--;
+		}
+	}
+	enque(value) {
+		const element = new Node(value);
+		if (this.size === 0) {
+			this.front = element;
+			this.rear = element;
+		} else {
+			element.next = this.front;
+			this.front.prev = element;
+			this.front = element;
+		}
+		this.size++;
+	}
+	deque() {
+		if (this.size !== 0) {
+			this.front = this.front.next;
+			if (this.front) {
+				this.front.prev = null;
+			}
+			this.size--;
+		}
+	}
+	getFront() {
+		return this.front ? this.front.value : null;
+	}
+	getRear() {
+		return this.rear ? this.rear.value : null;
+	}
+	getSize() {
+		return this.size;
+	}
 }
 
 module.exports = Deque;
