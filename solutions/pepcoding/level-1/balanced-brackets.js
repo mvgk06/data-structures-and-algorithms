8cfa6252431937b670c762be3772f03144510ff2
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-11-08 17:47:54 +0530

    added solution

---
+++ solutions/pepcoding/level-1/balanced-brackets.js
@@ -0,0 +1,68 @@
+/*
+
+Problem
+
+https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/balanced-brackets-official/ojquestion
+
+Approach
+- If the current element is an opening bracket, then push into the stack.
+- If the current element is a closing bracket and if the stack is empty, then return false.
+- Else if the top is not matching, then return false.
+- Else pop the opening bracket.
+- Once all the elements are processed, if the stack is not empty, then return false.
+- Else return true.
+
+Time - O(n)
+Space - O(n)
+
+n - size of string
+
+*/
+
+const isOpeningBracket = (ch) => {
+	return ch === '(' || ch === '[' || ch === '{';
+};
+
+const isClosingBracket = (ch) => {
+	return ch === ')' || ch === ']' || ch === '}';
+};
+
+const isMatchingBracket = (map, open, close) => {
+	return map[open] === close;
+};
+
+const balancedBrackets = (s) => {
+	const map = {
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	};
+	const stack = [];
+	let i = 0;
+	while (i < s.length) {
+		if (isOpeningBracket(s[i])) {
+			stack.push(s[i]);
+		} else if (isClosingBracket(s[i])) {
+			if (stack.length === 0) {
+				return false;
+			}
+			const top = stack[stack.length - 1];
+			if (!isMatchingBracket(map, top, s[i])) {
+				return false;
+			}
+			stack.pop();
+		}
+		i++;
+	}
+
+	if (stack.length !== 0) {
+		return false;
+	}
+
+	return true;
+};
+
+const solve = (s) => {
+	const result = balancedBrackets(s);
+	console.log(result);
+};
