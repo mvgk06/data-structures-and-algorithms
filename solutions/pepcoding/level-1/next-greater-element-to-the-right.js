104364dc3e68a5f323f2f1e5032230e29803b52e
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-11-12 10:27:44 +0530

    updated solution

--- solutions/pepcoding/level-1/next-greater-element-to-the-right.js
+++ solutions/pepcoding/level-1/next-greater-element-to-the-right.js
@@ -5,7 +5,7 @@ Problem
 https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/next-greater-element-official/ojquestion
 
 Approach 
-- Traverse the array from the right, while the stack is not empty and the top is smaller than the current element, pop the top of the stack.
+- Traverse the array from the right, while the stack is not empty and the top is smaller than or equal to the current element, pop the top of the stack.
 - If the stack is empty, then the result for the current index is -1, else the result is top of the stack.
 - Push the current element into the stack.
 - Print the result array.
@@ -21,7 +21,7 @@ const nextGreater = (n, arr) => {
 	const result = new Array(n);
 	const stack = [];
 	for (let i = n - 1; i >= 0; i--) {
-		while (stack.length !== 0 && stack[stack.length - 1] < arr[i]) {
+		while (stack.length !== 0 && stack[stack.length - 1] <= arr[i]) {
 			stack.pop();
 		}
 		if (stack.length === 0) {
--- solutions/pepcoding/level-1/stock-span.js
+++ solutions/pepcoding/level-1/stock-span.js
@@ -6,7 +6,7 @@ https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/sto
 
 Approach
 - We have to compute the number of elements between the current and nearest greatest element on the left.
-- While the stack is not empty and the top of stack is smaller than the current element, then pop the element.
+- While the stack is not empty and the top of stack is smaller than or equal to the current element, then pop the element.
 - If the stack is empty, then the result is index+1.
 - Else the result is index-top.
 - Push the current index into the stack.
@@ -23,7 +23,7 @@ const stockSpan = (n, arr) => {
 	const stack = [];
 	const result = new Array(n);
 	for (let i = 0; i < n; i++) {
-		while (stack.length !== 0 && arr[stack[stack.length - 1]] < arr[i]) {
+		while (stack.length !== 0 && arr[stack[stack.length - 1]] <= arr[i]) {
 			stack.pop();
 		}
 		if (stack.length === 0) {
