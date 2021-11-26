ad990b8e6fcbb466937d42e3bd7582fe801fc62b
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-11-26 16:59:18 +0530

    updated solution

--- solutions/leetcode/string-compression.js
+++ solutions/leetcode/string-compression.js
@@ -5,11 +5,11 @@ Problem
 https://leetcode.com/problems/string-compression/
 
 Approach
-- Initialize ith pointer as 0 and count as 1.
+- Initialize pointer i as 0, pointer j as 1 and len as 1.
 - Traverse the array.
-- If the current character is equal to the next character, then increment the count.
-- Else set the ith character as jth character, increment i, set the count of the character and reset the count to 1.
-- Return the ith pointer.
+- If the current character is equal to the previous character, then increment j and len.
+- Else set the character at i as character at j-1, increment i, set the len of the character, increment j and reset the len to 1.
+- Set the last character, increment i, set its len and return pointer i.
 
 Time - O(n)
 Space - O(n)
@@ -20,21 +20,33 @@ n - number of elements
 
 const compress = function (chars) {
 	let i = 0,
-		count = 1;
-	for (let j = 0; j < chars.length; j++) {
-		if (j + 1 < chars.length && chars[j] === chars[j + 1]) {
-			count++;
+		j = 1,
+		len = 1;
+	while (j < chars.length) {
+		if (chars[j] === chars[j - 1]) {
+			j++;
+			len++;
 		} else {
-			chars[i] = chars[j];
+			chars[i] = chars[j - 1];
 			i++;
-			if (count > 1) {
-				const s = count.toString();
+			if (len > 1) {
+				const s = len.toString();
 				for (let k = 0; k < s.length; k++) {
 					chars[i] = s[k];
 					i++;
 				}
 			}
-			count = 1;
+			j++;
+			len = 1;
+		}
+	}
+	chars[i] = chars[j - 1];
+	i++;
+	if (len > 1) {
+		const s = len.toString();
+		for (let k = 0; k < s.length; k++) {
+			chars[i] = s[k];
+			i++;
 		}
 	}
 	return i;
